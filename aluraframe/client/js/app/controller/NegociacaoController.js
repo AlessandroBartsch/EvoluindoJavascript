62b70cba1380740ecebdfa3bd78ce25235a3bb87
Alessandro Bartsch <alessandrobartsch94@gmail.com>
2021-03-31 08:32:31 -0300

    classe helper para data

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -11,17 +11,12 @@ class NegociacaoController {
 
     adiciona(event) {
         event.preventDefault();
-        
-        let data = new Date(...
-            this._inputData.value.split('-')
-            .map((item, indice) => item - indice % 2)
-            );
+        let dateHelper = new DateHelper();
 
+        let data = new Date(dateHelper.textoParaData(this._inputData.value));
+        let diaMesAno = dateHelper.dataParaTexto(data);
 
         let negociacao = new Negociacao(data, this._inputQuantidade.value, this._inputValor.value);
-        
-
-        let diaMesAno = negociacao.data.getDate() + "/" + (negociacao.data.getMonth() + 1) + "/" + negociacao.data.getFullYear();
 
         console.log(diaMesAno);
         console.log(negociacao);
