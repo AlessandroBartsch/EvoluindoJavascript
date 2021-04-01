721da40932c2cd2884cc60b198518ceadc7755c7
Alessandro Bartsch <alessandrobartsch94@gmail.com>
2021-04-01 15:01:05 -0300

    lista de negociacoes

--- aluraframe/client/js/app/controller/NegociacaoController.js
+++ aluraframe/client/js/app/controller/NegociacaoController.js
@@ -6,20 +6,32 @@ class NegociacaoController {
         this._inputData = $('#data');
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
+        this._listaNegociacao = new ListaNegociacoes();
 
     }
 
     adiciona(event) {
         event.preventDefault();
 
-        let negociacao = new Negociacao(
-            DateHelper.textoParaData(this._inputData.value), 
-            this._inputQuantidade.value, 
-            this._inputValor.value
-            );
-
-        console.log(negociacao);
-
+        this._listaNegociacao.adiciona(this._criaNegociacao());
+        this._limpaFormulario();
+        console.log(this._listaNegociacao);
     }
 
+
+    _criaNegociacao() {
+        return new Negociacao(
+            DateHelper.textoParaData(this._inputData.value), 
+            this._inputQuantidade.value, 
+            this._inputValor.value);
+    }
+
+    _limpaFormulario() {
+
+        this._inputData.value = '';
+        this._inputQuantidade.value = 1;
+        this._inputValor.value = 0,0;
+
+        this._inputData.focus();
+    }
 }
